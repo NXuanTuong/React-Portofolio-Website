3a8d9aca2c5bab22f97fb497e476955feffbd886
NXuanTuong <tuongbabynhok@gmail.com>
2022-12-31 02:19:04 +0700

    refactor code + code base UI

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,7 +4,7 @@ import './welcome.css'
 const Welcome = () => {
   return (
     <div id='body-welcome'>
-        <p className='title-welcome'>Welcome to my Nguyen Xuan Tuong profile: <br /> <a href="https://codepen.io/grohit/" class='link-github'>codepen.io/grohit</a></p>
+        <p className='title-welcome'>Welcome to my Nguyen Xuan Tuong profile: <br /> <a href="https://github.com/NXuanTuong/React-Portofolio-Website" class='link-github'>https://github.com/NXuanTuong/React-Portofolio-Website</a></p>
     </div>
   )
 }
